8a325bf71db08d07f7a42a01c67cd4a8e4a9e7f1
Robert Marsland <marsland@mit.edu>
2017-12-18 14:31:02 -0500

    More debugging for cluster
    Now I’ve actually tested it, and think it should really work.

--- community_simulator/trophic_calc.py
+++ community_simulator/trophic_calc.py
@@ -10,6 +10,7 @@ import argparse
 from community_simulator.cavity_threelevel import RunCommunity
 import numpy as np
 import distutils.dir_util
+import pandas as pd
 
 parser = argparse.ArgumentParser()
 parser.add_argument("task_ID", type=int)
@@ -18,8 +19,17 @@ parser.add_argument("scale", type=float)
 parser.add_argument("ns", type=int)
 args = parser.parse_args()
 
-folder = '/project/biophys/trophic_structure/dataDec17/vary_'+args.param
+folder = 'test'
+#folder = '/project/biophys/trophic_structure/dataDec17/vary_'+args.param
 distutils.dir_util.mkpath(folder)
+namelist = ['data','finalstate','simparams','cmatrix']
+filenamelist = [folder+'/'+namelist[q]+'_'+str(args.task_ID)+'_K_eta'+'.xlsx' 
+                for q in range(len(namelist))]
+ic = [0,[0,1,2],0,[0,1]]
+h = [0,0,[0,1],0]
+
+n_iter = 800
+trials = 27
 
 params = {'K':1.,
           'sigK':0.1,
@@ -41,51 +51,38 @@ S = 30
 Kvec = np.linspace(0,1.5,args.ns)
 etavec = np.linspace(0.3,5,args.ns)
 for j in range(len(Kvec)):
-    print('K='+Kvec[j])
+    print('K='+str(Kvec[j]))
     for m in range(len(etavec)):
         params['K']=Kvec[j]
         params['eta']=etavec[m]
-        data_new, final_state_new, sim_params_new, c_matrix_new= RunCommunity(params,S,trials=36,run_number=j,n_iter=800)
+        out = RunCommunity(params,S,trials=trials,run_number=j*len(etavec)+m,n_iter=n_iter)
     
-        if j==0 and k==0:
-            final_state = final_state_new.copy()
-            data = data_new.copy()
-            sim_params = sim_params_new.copy()
-            #c_matrix = c_matrix_new.copy()
+        if j==0 and m==0:
+            for q in range(4):
+                out[q].to_excel(filenamelist[q])
         else:
-            final_state = final_state.append(final_state_new.copy())
-            data = data.append(data_new.copy())
-            sim_params = sim_params.append(sim_params_new.copy())
-            #c_matrix = c_matrix.append(c_matrix_new.copy())
-        
-namelist = ['finalstate','data','simparams','cmatrix']
-j=0
-for item in [final_state,data,sim_params]:
-    item.to_csv(folder+'/'+namelist[j]+'_'+str(args.task_ID)+'_K_eta'+'.csv')
-    j+=1
+            for q in range(4):
+                pd.read_excel(filenamelist[q],index_col=ic[q],header=h[q]).append(out[q]).to_excel(filenamelist[q])
 
+        del out
+
+filenamelist = [folder+'/'+namelist[q]+'_'+str(args.task_ID)+'_muc_mud'+'.xlsx' 
+                for q in range(len(namelist))]
 mucvec = np.linspace(0.5,2.5,args.ns)
 mudvec = np.linspace(0.5,2,args.ns)
 for j in range(len(mucvec)):
-    print('muc='+mucvec[j])
+    print('muc='+str(mucvec[j]))
     for m in range(len(mudvec)):
         params['muc']=mucvec[j]
         params['mud']=mudvec[m]
-        data_new, final_state_new, sim_params_new, c_matrix_new= RunCommunity(params,S,trials=36,run_number=j,n_iter=800)
+        out = RunCommunity(params,S,trials=trials,run_number=j*len(mudvec)+m,n_iter=n_iter)
         
-        if j==0 and k==0:
-            final_state = final_state_new.copy()
-            data = data_new.copy()
-            sim_params = sim_params_new.copy()
-            #c_matrix = c_matrix_new.copy()
+        if j==0 and m==0:
+            for q in range(4):
+                out[q].to_excel(filenamelist[q])
         else:
-            final_state = final_state.append(final_state_new.copy())
-            data = data.append(data_new.copy())
-            sim_params = sim_params.append(sim_params_new.copy())
-            #c_matrix = c_matrix.append(c_matrix_new.copy())
+            for q in range(4):
+                pd.read_excel(filenamelist[q],index_col=ic[q],header=h[q]).append(out[q]).to_excel(filenamelist[q])
 
-namelist = ['finalstate','data','simparams','cmatrix']
-j=0
-for item in [final_state,data,sim_params]:
-    item.to_csv(folder+'/'+namelist[j]+'_'+str(args.task_ID)+'_muc_mud'+'.csv')
-    j+=1
+        del out
+
