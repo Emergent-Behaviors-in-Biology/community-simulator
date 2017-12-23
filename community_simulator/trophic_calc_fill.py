c2cc25a79992c3d4f9962dcb6a16e50cccb6f6b0
Robert Marsland <marsland@mit.edu>
2017-12-23 16:17:12 -0500

    simplified optimization
    Tweaked optimization scheme to get rid of unnecessary logs and to scale
    everything in a more reasonable way. Added new script to run new
    optimization automatically on all existing data.

---
+++ community_simulator/trophic_calc_fill.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 15 14:32:09 2017
+
+@author: robertmarsland
+"""
+
+import argparse
+from community_simulator.cavity_threelevel import RunCommunity, cost_function_bounded
+import numpy as np
+import pandas as pd
+from scipy.interpolate import griddata
+import scipy.optimize as opt
+import distutils.dir_util
+
+parser = argparse.ArgumentParser()
+parser.add_argument("task_ID", type=int)
+parser.add_argument("param", type=str)
+args = parser.parse_args()
+
+folder = '/project/biophys/trophic_structure/dataDec17/vary_'+args.param
+folder_new = '/project/biophys/trophic_structure/dataDec17/vary_'+args.param+'/new_opt'
+distutils.dir_util.mkpath(folder_new)
+filename = folder+'/'+'data_'+str(args.task_ID)+'_K_eta'+'.xlsx' 
+filename_new = folder_new+'/'+'data_'+str(args.task_ID)+'_K_eta'+'.xlsx' 
+data = pd.read_excel(filename,index_col=0)
+
+param_names = ['K','sigK','muc','sigc','mud','sigd','m','sigm','u','sigu','gamma','eta']
+arg_names = ['<R>','<N>','<X>','<R^2>','<N^2>','<X^2>']
+data_new = data.copy()
+
+for item in data.index:
+    params = data[param_names].loc[item].to_dict()
+    args = data[arg_names].loc[item].values
+    out = opt.minimize(cost_function_bounded,data[arg_names].loc[item],args=(params,))
+    if np.isfinite(out.fun):
+        data_new.loc[item,arg_names] = out.x
+        data_new.loc[item,'fun'] = out.fun
+        
+data_new.to_excel(filename_new)
