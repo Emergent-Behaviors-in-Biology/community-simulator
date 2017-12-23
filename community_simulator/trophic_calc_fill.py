67af378b4c0ab5baf541b41cd244e12058131bcd
Robert Marsland <marsland@mit.edu>
2017-12-23 16:50:39 -0500

    use interpolation to find starting values
    see if this gives better coverage

--- community_simulator/trophic_calc_fill.py
+++ community_simulator/trophic_calc_fill.py
@@ -30,11 +30,18 @@ filename = folder+'/'+'data_'+str(args.task_ID)+'_K_eta'+'.xlsx'
 filename_new = folder_new+'/'+'data_'+str(args.task_ID)+'_K_eta'+'.xlsx' 
 data = pd.read_excel(filename,index_col=0)
 data_new = data.copy()
+interp_data = data.copy()
+good_data = data[data['fun']<0.0001]
+for item in arg_names:
+    interp_data[item] = griddata(good_data[['K','eta']],good_data[item],data[['K','eta']])
 
 for item in data.index:
     params = data[param_names].loc[item].to_dict()
     args = data[arg_names].loc[item].values
-    out = opt.minimize(cost_function_bounded,data[arg_names].loc[item],args=(params,))
+    if np.all(np.isfinite(interp_data[arg_names].loc[item].values)):
+        out = opt.minimize(cost_function_bounded,interp_data[arg_names].loc[item].values,args=(params,))
+    else:
+        out = opt.minimize(cost_function_bounded,data[arg_names].loc[item].values,args=(params,))
     if np.isfinite(out.fun):
         data_new.loc[item,arg_names] = out.x
         data_new.loc[item,'fun'] = out.fun
