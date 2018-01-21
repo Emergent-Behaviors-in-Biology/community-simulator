11a12fe0915bb0b2a6895ec0857a78df462fe895
Robert Marsland <marsland@mit.edu>
2018-01-21 15:10:50 -0500

    Debug correction script
    Figured out how to modify table correctly in line 45/46.

--- community_simulator/trophic_calc_corr.py
+++ community_simulator/trophic_calc_corr.py
@@ -18,8 +18,8 @@ parser.add_argument("param", type=str)
 args = parser.parse_args()
 
 param_names = ['K','sigK','muc','sigc','mud','sigd','m','sigm','u','sigu','gamma','eta']
+#folder = '/Users/robertmarsland/Dropbox (Personal)/BU/Trophic Cavity Calc/vary_sigc_new'
 folder = '/project/biophys/trophic_structure/dataDec17_rev/vary_'+args.param
-distutils.dir_util.mkpath(folder)
 namelist = ['data','finalstate','simparams','cmatrix']
 filenamelist = [folder+'/'+namelist[q]+'_'+str(args.task_ID)+'_K_eta'+'.xlsx' for q in range(len(namelist))]
 ic = [0,[0,1,2],0,[0,1]]
@@ -28,21 +28,22 @@ h = [0,0,[0,1],[0,1]]
 old=[]
 for q in range(4):
     old.append(pd.read_excel(filenamelist[q],index_col=ic[q],header=h[q]))
+trials = len(old[1].keys())
 
-n_iter = 1000
-trials = 27
+n_iter = 200
 T=5
 cutoff = 1e-6
-S = 40
+
 
 for item in old[0].index:
+    S = int(old[0]['S'].loc[item])
     params = old[0][param_names].loc[item].to_dict()
     if params['K']>=0.7:
         out = RunCommunity(params,S,trials=trials,run_number=item,
                            n_iter=n_iter,T=T,cutoff=cutoff)
         
         for q in range(4):
-            old[q].loc[item]=out[q]
+            old[q].loc[item]=out[q].loc[item].values
             old[q].to_excel(filenamelist[q])
 
         del out
