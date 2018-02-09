aa3b8dcddf5871b95d18b56cbe5441ab67eb71fb
robertvsiii <robertvsiii@gmail.com>
2018-02-09 16:40:29 -0500

    new scaling script
    Edited script to allow full flexibility for taking the data I need.

--- community_simulator/model_scaling.py
+++ community_simulator/model_scaling.py
@@ -19,12 +19,19 @@ parser.add_argument("param", type=str)
 parser.add_argument("min", type=float)
 parser.add_argument("max", type=float)
 parser.add_argument("ns", type=int)
+parser.add_argument("extra_param", type=str)
+parser.add_argument("extra_param_val", type=float)
 parser.add_argument("n_iter", type=int)
 parser.add_argument("ind_trials", type=int)
 args = parser.parse_args()
 
+valid_params=['K' ,'q', 'e', 'fs', 'fw', 'food_type', 'Ddiv', 'n_types', 'c1', 'MA', 'SA', 'Sgen', 'S', 'n_wells']
+
+assert args.param in valid_params, 'Invalid choice of variable parameter.'
+assert args.extra_param in valid_params+['None'], 'Invalid choice of extra parameter.'
+
 #folder = 'test'
-folder = '/project/biophys/microbial_crm/'+args.param+'data'
+folder = '/project/biophys/microbial_crm/'+args.param+'data/'+args.extra_param
 distutils.dir_util.mkpath(folder)
 datanames = ['Consumers','Resources','Parameters','c_matrix','Realization']
 ic = [[0,1,2],[0,1,2],0,[0,1,2]]
@@ -32,16 +39,20 @@ h = [0,0,0,[0,1]]
 filenames = [folder+'/'+datanames[j]+'_'+str(datetime.datetime.now()).split()[0]+'.xlsx' for j in range(4)]
 filenames.append(folder+'/'+datanames[4]+'_'+str(datetime.datetime.now()).split()[0]+'.dat')
 
-trials = 10
 T=5
 
-extra_params = {'e':0.6}
+if args.extra_param != 'None':
+    extra_params = {args.extra_param:args.extra_param_val}
+else:
+    extra_params = {}
+if args.extra_param != 'n_wells':
+    extra_params['n_wells'] = 10
 
 paramvec=np.linspace(args.min,args.max,args.ns)
 for j in range(args.ns):
     for k in range(args.ind_trials):
         print(args.param+'='+str(paramvec[j]))
-        kwargs = {args.param:paramvec[j],'run_number':j*args.ind_trials+k,'n_iter':args.n_iter,'T':T,'n_wells':trials}
+        kwargs = {args.param:paramvec[j],'run_number':j*args.ind_trials+k,'n_iter':args.n_iter,'T':T}
         kwargs.update(extra_params)
         out = RunCommunity(**kwargs)
         if j==0 and k==0:
