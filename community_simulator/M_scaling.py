8b767b597986a53c2830a40c93bcc7347b9e0350
robertvsiii <robertvsiii@gmail.com>
2018-02-21 16:14:16 -0500

    New files to check scaling
    Do M scaling while keeping c1*M fixed. Also improved parameter saving, to avoid storing everything in memory.

--- community_simulator/M_scaling.py
+++ community_simulator/M_scaling.py
@@ -15,20 +15,9 @@ import datetime
 import pickle
 
 parser = argparse.ArgumentParser()
-parser.add_argument("param", type=str)
-parser.add_argument("min", type=float)
-parser.add_argument("max", type=float)
-parser.add_argument("ns", type=int)
-parser.add_argument("extra_param", type=str)
-parser.add_argument("extra_param_val", type=float)
 parser.add_argument("n_iter", type=int)
 parser.add_argument("ind_trials", type=int)
 args = parser.parse_args()
-
-valid_params=['K' ,'q', 'e', 'fs', 'fw', 'food_type', 'Ddiv', 'n_types', 'c1', 'MA', 'SA', 'Sgen', 'S', 'n_wells']
-
-assert args.param in valid_params, 'Invalid choice of variable parameter.'
-assert args.extra_param in valid_params+['None'], 'Invalid choice of extra parameter.'
 
 #folder = 'test'
 folder = '/project/biophys/microbial_crm/'+args.param+'data/'+args.extra_param
@@ -41,22 +30,18 @@ filenames.append(folder+'/'+datanames[4]+'_'+str(datetime.datetime.now()).split(
 
 T=5
 
-if args.extra_param != 'None':
-    extra_params = {args.extra_param:args.extra_param_val}
-else:
-    extra_params = {}
-if args.extra_param != 'n_wells':
-    extra_params['n_wells'] = 10
+#Hold p = muc/(c1*M) fixed
+MAvec = np.arange(12,25)
+c1vec = 25./MAvec
+ns = len(MAvec)
 
-paramvec=np.linspace(args.min,args.max,args.ns)
 f = open(filenames[4],'wb')
-for j in range(args.ns):
+for j in range(ns):
     for k in range(args.ind_trials):
-        print(args.param+'='+str(paramvec[j]))
-        kwargs = {args.param:paramvec[j],'run_number':j*args.ind_trials+k,'n_iter':args.n_iter,'T':T}
-        kwargs.update(extra_params)
+        kwargs = {'MA':MAvec[j],'c1':c1vec[j],'run_number':j*args.ind_trials+k,
+                  'n_iter':args.n_iter,'T':T,'extra_time':True,'n_wells':10}
         out = RunCommunity(**kwargs)
-        pickle.dump([out[4]],f)
+        pickle.dump(out[4],f)
         if j==0 and k==0:
             for q in range(4):
                 out[q].to_excel(filenames[q])
