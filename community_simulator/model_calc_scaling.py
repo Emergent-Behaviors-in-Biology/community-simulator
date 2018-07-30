511645f7fab1207252aaa78f17c06cec5e32610f
Robert Marsland <marsland@mit.edu>
2018-07-30 16:42:21 -0400

    edited date
    avoid filename confusion

--- community_simulator/model_calc_scaling.py
+++ community_simulator/model_calc_scaling.py
@@ -26,7 +26,9 @@ datanames = ['Consumers','Resources','Parameters','Initial_State','Realization']
 suff = ['.xlsx']*4+['.dat']
 ic = [[0,1,2],[0,1,2],0]
 h = [0,0,0]
-filenames = [folder+'/'+datanames[j]+'_'+str(datetime.datetime.now()).split()[0]+'_'+str(args.task_ID)+suff[j] for j in range(5)]
+date = str(datetime.datetime.now()).split()[0]
+date = '2018-07-31'
+filenames = [folder+'/'+datanames[j]+'_'+date+'_'+str(args.task_ID)+suff[j] for j in range(5)]
 
 #ITERATIONS, ETC.
 n_iter = 100
