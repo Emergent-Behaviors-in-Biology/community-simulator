fixed single carbon source calculation

community_simulator/cavity_crossfeeding.py
@@ -31,21 +31,23 @@ def y(Delta):
 
 #Standard deviations of invasion growth rates
 def sigN(args,params):
-    R,N,qR,qN = args
+    R,N,qR = args
     return np.sqrt(params['sigm']**2 + (1-params['l'])**2*params['sigc']**2
                   *(params['mug']**2+params['sigg']**2)*qR)
 def sigd(args,params):
-    R,N,qR,qN = args
+    R,N,qR = args
+    qN = y(DelN(args,params))*N**2
     return np.sqrt(params['sigw']**2 + params['sigc']**2*qN/params['gamma'])
 def sigp(args,params):
-    R,N,qR,qN = args
+    R,N,qR = args
+    qN = y(DelN(args,params))*N**2
     return np.sqrt(params['l']**2*(params['sigc']**2*params['sigD']**2*qR*qN+
                                    params['muc']**2*params['sigD']**2*N**2*qR/params['gamma'])
                                    /params['gamma'])
 
 #Mean invasion growth rates normalized by standard deviation
 def DelN(args,params):
-    R,N,qR,qN = args
+    R,N,qR = args
     return ((1-params['l'])*params['muc']*params['mug']*R-params['m'])/sigN(args,params)
 
 #Fraction of species that survive in steady state
@@ -54,7 +56,7 @@ def phiN(args,params):
 
 #Susceptibilities
 def nu(args,params):
-    R,N,qR,qN = args
+    R,N,qR = args
     omega_eff = params['omega']+params['muc']*N/params['gamma']
     kappa_eff = params['kappa']+params['l']*params['muc']*N*R/params['gamma']
     r2 = kappa_eff*phiN(args,params)/(params['gamma']*omega_eff*R)
@@ -65,7 +67,7 @@ def nu(args,params):
     return nu0*(1-(6*r1**2*eps_pk-(2*r1-1)*eps_d)/(6*r1**3*r2**(-2)+r1**2*r2**(-2)-16*r1**2-8*r1))
 
 def chi(args,params):
-    R,N,qR,qN = args
+    R,N,qR = args
     omega_eff = params['omega']+params['muc']*N/params['gamma']
     kappa_eff = params['kappa']+params['l']*params['muc']*N*R/params['gamma']
     r2 = kappa_eff*phiN(args,params)/(params['gamma']*omega_eff*R)
@@ -78,7 +80,7 @@ def chi(args,params):
 
 #Factors for converting invasion growth rate to steady-state abundance
 def fN(args,params):
-    R,N,qR,qN = args
+    R,N,qR = args
     return ((1-params['l'])*params['mug']*chi(args,params)*params['sigc']**2*R)**(-1)
 
 #Test satisfaction of competitive exclusion bound for consumers and predators
@@ -86,7 +88,7 @@ def test_bound_1(args,params):
     return params['gamma']-phiN(args,params)
 
 def cost_vector(args,params):
-    R,N,qR,qN = args
+    R,N,qR = args
     omega_eff = params['omega']+params['muc']*N/params['gamma']
     kappa_eff = params['kappa']+params['l']*params['muc']*N*R/params['gamma']
     nubar = nu(args,params)*(1-params['l'])*params['mug']*params['sigc']**2/params['gamma']
@@ -96,8 +98,23 @@ def cost_vector(args,params):
 
     RHS = np.asarray([(omega_eff/(2*nubar))*(np.sqrt(1+4*r1)-1-2*(eps_pw-r1*eps_d)/(1+4*r1)**(3/2)),
                      fN(args,params)*sigN(args,params)*w1(DelN(args,params)),
-                     (omega_eff**2/(2*nubar**2))*(1+2*r1-np.sqrt(1+4*r1)+eps_d*(1-(1+6*r1)/((1+4*r1)**(3/2)))+2*eps_pw/(1+4*r1)**(3/2)),
-                     (fN(args,params)*sigN(args,params))**2*w2(DelN(args,params))])
+                     (omega_eff**2/(2*nubar**2))*(1+2*r1-np.sqrt(1+4*r1)+eps_d*(1-(1+6*r1)/((1+4*r1)**(3/2)))+2*eps_pw/(1+4*r1)**(3/2))])
+    
+    return RHS
+
+def cost_vector_single(args,params):
+    R,qR = args
+    N = params['N']
+    args = np.asarray([R,N,qR])
+    omega_eff = params['omega']+params['muc']*N/params['gamma']
+    kappa_eff = params['kappa']+params['l']*params['muc']*N*R/params['gamma']
+    nubar = nu(args,params)*(1-params['l'])*params['mug']*params['sigc']**2/params['gamma']
+    r1 = kappa_eff*nubar/omega_eff**2
+    eps_pw = sigp(args,params)**2*nubar**2/omega_eff**4
+    eps_d = sigd(args,params)**2/omega_eff**2
+
+    RHS = np.asarray([(omega_eff/(2*nubar))*(np.sqrt(1+4*r1)-1-2*(eps_pw-r1*eps_d)/(1+4*r1)**(3/2)),
+                     (omega_eff**2/(2*nubar**2))*(1+2*r1-np.sqrt(1+4*r1)+eps_d*(1-(1+6*r1)/((1+4*r1)**(3/2)))+2*eps_pw/(1+4*r1)**(3/2))])
     
     return RHS
 
@@ -105,6 +122,10 @@ def cost_vector(args,params):
 def cost_function(args,params):
     args = np.exp(args)
     return np.sum((args-cost_vector(args,params))**2)
+
+def cost_function_single(args,params):
+    args = np.exp(args)
+    return np.sum((args-cost_vector_single(args,params))**2)
 
 #Enforce competitive exclusion bounds and keep moments within reasonable values
 # def cost_function_bounded(args,params):
@@ -159,19 +180,23 @@ def RunCommunity(assumptions,M,eps=1e-5,trials=1,postprocess=True,
     
     if assumptions['single']:
         #Get closed-form solution for large <N> limit of single
-        args_closed = [np.nan,np.nan,np.nan,np.nan]
+        args_closed = np.asarray([np.nan,np.nan,np.nan])
         y0 = assumptions['muc']**2*((1/assumptions['l'])-1)/(assumptions['gamma']*assumptions['sigc']**2)
         out = minimize_scalar(lambda Delta:(y(Delta)-y0)**2,bracket=[-5,5])
-        DelN = out.x
+        #r2 = lambda Delta: assumptions['l']*w0(Delta)/assumptions['gamma']
+        #y0 = lambda Delta: (assumptions['muc']**2/(assumptions['gamma']*assumptions['sigc']**2))*((1/assumptions['l'])-(1-r2(Delta)))*(1+4*r2(Delta))**(3/2)
+        #y0 = lambda Delta: (assumptions['muc']**2/(assumptions['gamma']*assumptions['sigc']**2))*(r2(Delta)*assumptions['sigD']**2/(assumptions['l']-assumptions['sigD']**2) + ((1/assumptions['l'])-(1-r2(Delta)))*(1+4*r2(Delta))**(3/2))/(1-r2(Delta)*assumptions['sigD']**2/(1-r2(Delta)*assumptions['sigD']**2/(assumptions['l']-assumptions['sigD']**2)))
+        #out = minimize_scalar(lambda Delta:(y(Delta)-y0(Delta))**2,bracket=[-5,5])
+
+        DelN_closed = out.x
         R0 = assumptions['m']/((1-assumptions['l'])*assumptions['mug']*assumptions['muc'])
-        r3 = assumptions['sigc']*DelN/(assumptions['muc']*np.sqrt(assumptions['l']-assumptions['sigD']**2))
-        r4 = assumptions['sigm']*DelN/assumptions['m']
+        r3 = assumptions['sigc']*DelN_closed/(assumptions['muc']*np.sqrt(assumptions['l']-assumptions['sigD']**2))
+        r4 = assumptions['sigm']*DelN_closed/assumptions['m']
         R_closed = R0*(1+r3*np.sqrt(1-r4**2))/(1-r3**2)
         qR_closed = (R_closed**2)/(assumptions['l']-assumptions['sigD']**2)
         args_closed[0] = R_closed
         args_closed[2] = qR_closed
         args_closed[1] = assumptions['R0']/(S*assumptions['m'])
-        args_closed[3] = y(DelN)*(args_closed[1]**2)
         assumptions['kappa'] = 0
     else:
         assumptions['kappa'] = np.mean(assumptions['R0']/assumptions['tau'])
@@ -192,182 +217,74 @@ def RunCommunity(assumptions,M,eps=1e-5,trials=1,postprocess=True,
         Rmean = TestPlate.R.mean(axis=0)
         R2mean = (TestPlate.R**2).mean(axis=0)
         Nmean = (Stot*1./S)*TestPlate.N.mean(axis=0)
-        N2mean = (Stot*1./S)*(TestPlate.N**2).mean(axis=0)
     
         #Compute moments for feeding in to cavity calculation
         args0 = np.asarray([np.mean(Rmean),
                             np.mean(Nmean),
-                            np.mean(R2mean),
-                            np.mean(N2mean)])+1e-10
+                            np.mean(R2mean)])+1e-10
         args0_err = np.asarray([np.std(Rmean),
                                 np.std(Nmean),
-                                np.std(R2mean),
-                                np.std(N2mean)])
+                                np.std(R2mean)])
         
-        out = opt.minimize(cost_function,np.log(args0),args=(assumptions,),bounds=(
-            (-10,10),(-10,10),(-10,10),(-10,10)))
-        args_cav = np.exp(out.x)
-        fun = out.fun/np.sum(args0**2)
+        if assumptions['single']:
+            bounds = [(np.log(args0[k])-1,np.log(args0[k])+1) for k in [0,2]]
+            assumptions['N'] = args0[1]
+            out = opt.minimize(cost_function_single,np.log(args0[[0,2]]),args=(assumptions,),bounds=bounds,tol=1e-8)
+            args_cav = np.exp(out.x)
+            args_cav = [args_cav[0],args0[1],args_cav[1]]
+        else:
+            bounds = [(np.log(args_closed[k])-1,np.log(args_closed[k])+1) for k in range(3)]
+            out = opt.minimize(cost_function,np.log(args_closed),args=(assumptions,),bounds=bounds,tol=1e-8)
+            #ranges = [(np.log(args_closed[k])-1,np.log(args_closed[k])+1) for k in range(4)]
+            #out = opt.brute(cost_function,ranges,Ns=100,args=(assumptions,),workers=-1)
+            args_cav = np.exp(out.x)
+        fun = out.fun#/np.sum(args0**2)
         k += 1
     if fun > eps:
-        args_cav = [np.nan,np.nan,np.nan,np.nan]
+        args_cav = [np.nan,np.nan,np.nan]
         fun = np.nan
     
     if postprocess:
         results_num = {'SphiN':np.sum(TestPlate.N.values.reshape(-1)>cutoff)*1./trials,
                        'M<R>':M*args0[0],
                        'S<N>':S*args0[1],
-                       'MqR':M*args0[2],
-                       'SqN':S*args0[3]}
+                       'MqR':M*args0[2]}
         results_cav = {'SphiN':S*phiN(args_cav,assumptions),
                        'M<R>':M*args_cav[0],
                        'S<N>':S*args_cav[1],
-                       'MqR':M*args_cav[2],
-                       'SqN':S*args_cav[3]}
+                       'MqR':M*args_cav[2]}
         if assumptions['single']:
-            results_closed = {'SphiN':S*w0(DelN),
+            results_closed = {'SphiN':S*w0(DelN_closed),
                             'M<R>':M*args_closed[0],
                             'S<N>':S*args_closed[1],
-                            'MqR':M*args_closed[2],
-                            'SqN':S*args_closed[3]}
+                            'MqR':M*args_closed[2]}
         else:
             results_closed = np.nan
-        return results_num, results_cav, results_closed, out, args0, assumptions
+        return results_num, results_cav, results_closed, out, args0, assumptions, TestPlate
 
     else:
 
-        data = pd.DataFrame([args_cav],columns=['<R>','<N>','<R^2>','<N^2>'],index=[run_number])
+        data = pd.DataFrame([args_cav],columns=['<R>','<N>','<R^2>'],index=[run_number])
         data['fun']=fun
         for item in assumptions.keys():
             data[item]=assumptions[item]
         data['S'] = S
         data['M'] = M
         data['phiN'] = phiN(args_cav,assumptions)
+        data['<N^2>'] = y(DelN(args_cav,assumptions))*args_cav[1]**2
 
-        data_sim = pd.DataFrame([args0],columns=['<R>','<N>','<R^2>','<N^2>'],index=[run_number])
+        data_sim = pd.DataFrame([args0],columns=['<R>','<N>','<R^2>'],index=[run_number])
         data_sim['phiN'] = np.mean((TestPlate.N>cutoff).sum(axis=0))/S
-        err_sim = pd.DataFrame([args0_err],columns=['<R>','<N>','<R^2>','<N^2>'],index=[run_number])
-        err_sim['phiN'] = np.std((TestPlate.N>cutoff).sum(axis=0))
+        data_sim['<N^2>'] = np.mean((Stot*1./S)*(TestPlate.N**2).mean(axis=0))
+        err_sim = pd.DataFrame([args0_err],columns=['<R>','<N>','<R^2>'],index=[run_number])
+        err_sim['phiN'] = np.std((TestPlate.N>cutoff).sum(axis=0))/S
+        err_sim['<N^2>'] = np.std((Stot*1./S)*(TestPlate.N**2).mean(axis=0))
         data = data.join(data_sim,rsuffix='_sim').join(err_sim,rsuffix='_sim_err')
 
         if assumptions['single']:
-            data_closed = pd.DataFrame([args_closed],columns=['<R>','<N>','<R^2>','<N^2>'],index=[run_number])
-            data_closed['phiN'] = w0(DelN)
+            data_closed = pd.DataFrame([args_closed],columns=['<R>','<N>','<R^2>'],index=[run_number])
+            data_closed['phiN'] = w0(DelN_closed)
+            data_closed['<N^2>'] = y(DelN_closed)*args_closed[1]**2
             data = data.join(data_closed,rsuffix='_closed')
 
         return data
-
-#############################
-#STUDY RESULTS
-#############################
-
-param_names = 'K,sigK,muc,sigc,mud,sigd,m,sigm,u,sigu,gamma,eta,epsN,epsX'.split(',')
-idx=pd.IndexSlice
-    
-def FormatPath(folder):
-    if folder==None:
-        folder=''
-    else:
-        if folder != '':
-            if folder[-1] != '/':
-                folder = folder+'/'
-    return folder
-
-def LoadData(folder,task_id = 1,load_all = False, suff = 'K_eta'):
-    folder = FormatPath(folder)
-    name = '_'+str(task_id)+'_'+suff+'.xlsx'
-    finalstate = pd.read_excel(folder+'finalstate'+name,index_col=[0,1,2],header=[0])
-    params = pd.read_excel(folder+'data'+name,index_col=[0],header=[0])
-    
-    if load_all:
-        c = pd.read_excel(folder+'cmatrix'+name,index_col=[0,1],header=[0,1])
-        simparams = pd.read_excel(folder+'simparams'+name,index_col=[0],header=[0,1])
-        return finalstate,params,simparams,c
-    else:
-        return finalstate,params
-
-def ComputeIPR(df):
-    IPR = pd.DataFrame(columns=df.keys(),index=df.index.levels[0])
-    for j in df.index.levels[0]:
-        p = df.loc[j]/df.loc[j].sum()
-        IPR.loc[j] = 1./((p[p>0]**2).sum())
-    return IPR
-
-def PostProcess(folders,tmax=10,tmin=1,suff='K',thresh=1e-4):
-    j=0
-    data_names = ['Herbivore IPR','Plant IPR','Carnivore IPR',
-                 'Herbivore richness','Plant richness','Carnivore richness',
-                 'Herbivore biomass','Plant biomass','Carnivore biomass']
-    data_names = data_names + [name+' Error' for name in data_names]
-                        
-    for k in np.arange(len(folders)):
-        folder = FormatPath(folders[k])
-
-        for task_id in np.arange(tmin,tmax+1):
-            data = pd.DataFrame(index=[0],columns=data_names+param_names)
-            finalstate,params = LoadData(folder,task_id=task_id, suff=suff)
-            N = finalstate.loc[idx[:,'Consumer',:],:]
-            R = finalstate.loc[idx[:,'Resource',:],:]
-            X = finalstate.loc[idx[:,'Predator',:],:]
-            
-            N_IPR = ComputeIPR(N)
-            R_IPR = ComputeIPR(R)
-            X_IPR = ComputeIPR(X)
-            n_wells = len(N_IPR.keys())
-            
-            for plate in N_IPR.index:
-                data.loc[j,'Herbivore IPR'] = N_IPR.loc[plate].mean()
-                data.loc[j,'Plant IPR'] = R_IPR.loc[plate].mean()
-                data.loc[j,'Carnivore IPR'] = X_IPR.loc[plate].mean()
-                data.loc[j,'Herbivore richness']=(N.loc[plate]>thresh).sum().mean()
-                data.loc[j,'Plant richness']=(R.loc[plate]>thresh).sum().mean()
-                data.loc[j,'Carnivore richness']=(X.loc[plate]>thresh).sum().mean()
-                data.loc[j,'Herbivore biomass']=N.loc[plate].sum().mean()
-                data.loc[j,'Plant biomass']=R.loc[plate].sum().mean()
-                data.loc[j,'Carnivore biomass']=X.loc[plate].sum().mean()
-                data.loc[j,'Herbivore IPR Error'] = N_IPR.loc[plate].std()/np.sqrt(n_wells)
-                data.loc[j,'Plant IPR Error'] = R_IPR.loc[plate].std()/np.sqrt(n_wells)
-                data.loc[j,'Carnivore IPR Error'] = X_IPR.loc[plate].std()/np.sqrt(n_wells)
-                data.loc[j,'Herbivore richness Error']=(N.loc[plate]>thresh).sum().std()/np.sqrt(n_wells)
-                data.loc[j,'Plant richness Error']=(R.loc[plate]>thresh).sum().std()/np.sqrt(n_wells)
-                data.loc[j,'Carnivore richness Error']=(X.loc[plate]>thresh).sum().std()/np.sqrt(n_wells)
-                data.loc[j,'Herbivore biomass Error']=N.loc[plate].sum().std()/np.sqrt(n_wells)
-                data.loc[j,'Plant biomass Error']=R.loc[plate].sum().std()/np.sqrt(n_wells)
-                data.loc[j,'Carnivore biomass Error']=X.loc[plate].sum().std()/np.sqrt(n_wells)
-                for item in param_names:
-                    data.loc[j,item] = params.loc[plate,item]
-                j+=1
-            data.to_excel('processed_data_'+str(task_id)+'.xlsx')
-    return data
-
-def ReviveCommunity(folder,task_id=1,run_number=1,wells=[]):
-    finalstate,params,simparams,c=LoadData(folder,task_id=task_id,load_all=True)
-    Nold = finalstate.loc[run_number].loc['Consumer']
-    Rold = finalstate.loc[run_number].loc[['Resource','Predator']]
-    S = len(Nold)
-    M = len(Rold)
-    if len(wells)==0:
-        init_state = [Nold,Rold]
-    else:
-        init_state = [Nold[wells],Rold[wells]]
-
-    assumptions = {'regulation':'independent','replenishment':'predator','response':'type I'}
-    def dNdt(N,R,params):
-        return usertools.MakeConsumerDynamics(**assumptions)(N,R,params)
-    def dRdt(N,R,params):
-        return usertools.MakeResourceDynamics(**assumptions)(N,R,params)
-    dynamics = [dNdt,dRdt]
-
-    R0 = simparams.loc[run_number].loc['R0']
-    u = simparams.loc[run_number].loc['u']
-    params={'c':c.loc[run_number],
-            'm':simparams.loc[run_number].loc['m'],
-            'u':np.hstack([np.zeros(len(R0)),u]),
-            'w':1,
-            'g':1,
-            'e':1,
-            'R0':np.hstack([R0,np.zeros(len(u))]),
-            'r':np.hstack([np.ones(len(R0)),np.zeros(len(u))])
-           }
-
-    return init_state,dynamics,params